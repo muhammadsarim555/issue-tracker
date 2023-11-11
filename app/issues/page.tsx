e9d122e8f3e20316991c1675dc4165170f27c638
Muhammad Sarim <36129452+muhammadsarim555@users.noreply.github.com>
2023-11-11 22:36:56 +0500

    radix ui added

---
+++ app/issues/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "@radix-ui/themes";
+
+function IssuesPage() {
+  return (
+    <div>
+      <Button> Issues Page</Button>
+    </div>
+  );
+}
+
+export default IssuesPage;
